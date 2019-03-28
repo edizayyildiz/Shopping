82460c521034ceda6bc1cd2d6085938382497ca4
Murat Demirci <mdemirci@outlook.com>
2019-03-28 14:59:44 +0300

    arayüz katmanı hazırlanıyor

---
+++ Shopping.Web/AutofacModule.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using Shopping.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Shopping.Web
+{
+    public class AutofacModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(Assembly.Load("Shopping.Service"))
+                .AsClosedTypesOf(typeof(ICommandHandler<>)).InstancePerDependency();
+        }
+    }
+}
