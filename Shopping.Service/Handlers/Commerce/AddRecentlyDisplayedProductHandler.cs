7fc7c90815139a8286084ef503d1f0fe931cfd8d
smeyyekeskn <smeyyekeskn@gmail.com>
2019-04-01 14:15:30 +0300

    Ürüne son görüntülenme alanı eklendi commandleri ve servisleri yapıldı. Ve anasayfada bazı türkçeleştirmeler yapıldı

---
+++ Shopping.Service/Handlers/Commerce/AddRecentlyDisplayedProductHandler.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using Shopping.Data;
+using Shopping.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping.Service.Handlers
+{
+    public class AddRecentlyDisplayedProductHandler : CommandHandler<Commands.AddRecentlyDisplayedProduct>
+    {
+        private readonly IRepository<RecentlyDisplayedProduct> repository;
+        private readonly IUnitOfWork unitOfWork;
+        public AddRecentlyDisplayedProductHandler(IUnitOfWork unitOfWork, IRepository<RecentlyDisplayedProduct> repository)
+        {
+            this.unitOfWork = unitOfWork;
+            this.repository = repository;
+        }
+        public override async Task<dynamic> HandleAsync(Commands.AddRecentlyDisplayedProduct command)
+        {
+            Result result;
+            // validate the command
+            if (string.IsNullOrWhiteSpace(command.UserName))
+            {
+                result = new Result(false, command.UserName, "Kullanıcı adı gereklidir.", true, null);
+                return await Task.FromResult(result);
+            }
+            if (command.UserName.Length > 100)
+            {
+                result = new Result(false, command.UserName, "Kullanıcı adı 100 karakterden uzun olamaz.", true, null);
+                return await Task.FromResult(result);
+            }
+
+
+            // map command to the model
+            var model = Mapper.Map<RecentlyDisplayedProduct>(command);
+
+            // mark the model to insert
+            repository.Insert(model);
+
+            // save changes to database
+            await unitOfWork.SaveChangesAsync();
+
+            // return the result
+            result = new Result(true, model.Id, "Ürünün son görüntülenmesi eklendi.", true, 1);
+            return await Task.FromResult(result);
+        }
+    }
+}
---
+++ Shopping.Service/Handlers/Commerce/GetRecentlyDisplayedProductHandler.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Shopping.Data;
+using Shopping.Model.Entities;
+using Shopping.Service.Queries;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping.Service.Handlers
+{
+    public class GetRecentlyDisplayedProductHandler : CommandHandler<Commands.GetRecentlyDisplayedProduct>
+    {
+        private readonly IRepository<RecentlyDisplayedProduct> repository;
+        public GetRecentlyDisplayedProductHandler(IRepository<RecentlyDisplayedProduct> repository)
+        {
+            this.repository = repository;
+        }
+        public override async Task<dynamic> HandleAsync(Commands.GetRecentlyDisplayedProduct command)
+        {
+            // get the model from database
+            Result result;
+            var model = await repository.GetAsync(command.Id);
+
+            // if nothing found
+            if (model == null)
+            {
+                // return the not found result
+                result = new Result(false, command.Id, "Ürün son görüntülenmesi bulunamadı.", true, null);
+                return await Task.FromResult(result);
+            }
+            // map the model to query
+            var value = Mapper.Map<RecentlyDisplayedProductQuery>(model);
+
+            // return the query result
+            result = new Result(true, value, "1 adet ürünün son görüntülenmesi bulundu.", true, 1);
+            return await Task.FromResult(result);
+        }
+    }
+}
